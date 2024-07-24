25aae6496fc888b1a764c28ee6f7134050372487
jetsada117 <jornny.prombut@gmail.com>
2024-07-24 22:40:02 +0700

    edit 2

--- Home.java
+++ Home.java
@@ -16,26 +16,26 @@ class MyFrame extends JFrame{
     JPanel pan3 = new JPanel();
     JPanel pink = new JPanel();
     public MyFrame() {
-        setSize(1960,1080);
-        setLocation(0,0);
+        setSize(900,600);
+        setLocation(400,200);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(null);
         
         pink.setSize(getWidth(),getHeight());
         pink.setLocation(0,0); 
         pink.setBackground(Color.PINK);        
+
+        pan2.setSize(getWidth()*30/100,getHeight()*70/100);
+        pan2.setLocation(0,0);
+        pan2.setBackground(Color.yellow);        
         
-        pan1.setSize(1200,745);
-        pan1.setLocation(705,0);        
+        pan1.setSize(getWidth()*67/100,getHeight()*70/100);
+        pan1.setLocation(pan2.getWidth()+5,0);        
         pan1.setBackground(Color.BLACK);
         pan1.setLayout(new GridLayout(10,20));
 
-        pan2.setSize(700,745);
-        pan2.setLocation(0,0);
-        pan2.setBackground(Color.yellow);
-
-        pan3.setSize(1960,300);
-        pan3.setLocation(0,750);
+        pan3.setSize(getWidth(),getHeight()*25/100);
+        pan3.setLocation(0,pan1.getHeight()+5);
         pan3.setBackground(Color.blue);
 
         add(pan1);
