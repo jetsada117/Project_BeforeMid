730fab9b62fa37be9fc3783d2283ff3cd8fa79d5
natt06590 <a0852939405@gmail.com>
2024-07-26 22:36:34 +0700

    Add files via upload

---
+++ set_ButtonOrganizer.java
@@ -0,0 +1,14 @@
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class set_ButtonOrganizer extends JButton{
+    
+    public set_ButtonOrganizer(){
+        setFont(new Font("Arial", Font.BOLD, 20));
+        setSize(200, 60);
+        setLocation(350, 250);
+        setText("ORGANIZER");
+    }
+}
---
+++ work.java
@@ -0,0 +1,60 @@
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.Font;
+import java.awt.Image;
+
+public class work {
+    public static void main(String[] in){
+        MyFramework frame = new MyFramework();
+        frame.setVisible(true);  
+
+    }
+}
+class MyFramework extends JFrame{
+    JLabel jLabel = new JLabel("PM 2.5");
+
+    ImageIcon img = new ImageIcon("Background_Menu.jpg");
+    Image image1 = img.getImage();
+    Image image2 = image1.getScaledInstance(900,600,Image.SCALE_SMOOTH);
+    ImageIcon imageIcon1 = new ImageIcon(image2);
+    JLabel jLabelImg = new JLabel(imageIcon1);
+    JPanel jPanel1 = new JPanel();
+
+    public MyFramework(){
+        setSize(900,600);
+        setLocation(400,200);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(null);
+
+        jPanel1.setSize(900,600);
+        jPanel1.setLocation(0,0);
+        jPanel1.setBackground(Color.WHITE);
+
+        
+        set_ButtonStart ButtonStart = new set_ButtonStart();
+        set_ButtonOrganizer buttonoborganizer = new set_ButtonOrganizer();
+        set_Logout buttonend = new set_Logout();
+        
+        
+        jLabel.setSize(200, 120);
+        jLabel.setLocation(350, 20);
+        jLabel.setFont(new Font("Arial", Font.BOLD, 60));
+
+
+
+        jPanel1.add(jLabelImg);
+        
+        add(jLabel);
+        add(buttonend);
+        add(buttonoborganizer);
+        add(ButtonStart);
+        add(jPanel1);
+
+        // ้ำำสสน ไนพสกujdsfh89u idsfrgt iuy
+        
+    }
+}
+
