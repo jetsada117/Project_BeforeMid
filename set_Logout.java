98353f8a2656eb01ad4be41e3466bff317280b2e
natt06590 <a0852939405@gmail.com>
2024-07-26 22:37:17 +0700

    Add files via upload

---
+++ set_Logout.java
@@ -0,0 +1,15 @@
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+public class set_Logout extends JButton {
+   
+    public set_Logout(){
+        setFont(new Font("Arial", Font.BOLD, 20));
+        setSize(200, 60);
+        setLocation(350, 350);
+        setText("EXIT");
+    }
+    
+}
