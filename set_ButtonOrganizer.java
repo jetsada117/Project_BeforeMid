de0a2a311431fafdb2d7f7b17ac94e0aba15a507
natt06590 <a0852939405@gmail.com>
2024-07-26 21:52:57 +0700

    Add Background

--- set_ButtonOrganizer.java
+++ set_ButtonOrganizer.java
@@ -3,9 +3,9 @@ import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-public class setset_Buttonmember extends JButton{
+public class set_ButtonOrganizer extends JButton{
     
-    public setset_Buttonmember(){
+    public set_ButtonOrganizer(){
         setFont(new Font("Arial", Font.BOLD, 20));
         setSize(200, 60);
         setLocation(350, 250);
--- work.java
+++ work.java
@@ -1,11 +1,11 @@
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.FlowLayout;
 
+import java.awt.Color;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import java.awt.Font;
+import java.awt.Image;
 
 public class work {
     public static void main(String[] in){
@@ -17,25 +17,43 @@ public class work {
 class MyFramework extends JFrame{
     JLabel jLabel = new JLabel("PM 2.5");
 
-   
+    ImageIcon img = new ImageIcon("Background_Menu.jpg");
+    Image image1 = img.getImage();
+    Image image2 = image1.getScaledInstance(900,600,Image.SCALE_SMOOTH);
+    ImageIcon imageIcon1 = new ImageIcon(image2);
+    JLabel jLabelImg = new JLabel(imageIcon1);
+    JPanel jPanel1 = new JPanel();
+
     public MyFramework(){
         setSize(900,600);
         setLocation(400,200);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(null);
 
-        set_ButtonStart B1 = new set_ButtonStart();
-        setset_Buttonmember b2 = new setset_Buttonmember();
-        set_Logout B3 = new set_Logout();
+        jPanel1.setSize(900,600);
+        jPanel1.setLocation(0,0);
+        jPanel1.setBackground(Color.WHITE);
+
+        
+        set_ButtonStart ButtonStart = new set_ButtonStart();
+        set_ButtonOrganizer buttonoborganizer = new set_ButtonOrganizer();
+        set_Logout buttonend = new set_Logout();
+        
+        
         jLabel.setSize(200, 120);
         jLabel.setLocation(350, 20);
         jLabel.setFont(new Font("Arial", Font.BOLD, 60));
+
+
+
+        jPanel1.add(jLabelImg);
         
         add(jLabel);
-        add(B3);
-        add(b2);
-        add(B1);
-       
+        add(buttonend);
+        add(buttonoborganizer);
+        add(ButtonStart);
+        add(jPanel1);
         
     }
 }
+
